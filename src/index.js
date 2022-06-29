cf4c06d3355753a50eecb92ea2a4bc6d80d3ee7c
kok-s0s <2694308562@qq.com>
2022-06-29 23:42:06 +0800

    feat: highlight current selected item

--- src/index.js
+++ src/index.js
@@ -105,7 +105,7 @@ class Game extends React.Component {
     const moves = history.map((step, move) => {
       const desc = move ? 'Go to move #' + move : 'Go to game start'
       return (
-        <li key={move}>
+        <li key={move} className={this.state.stepNumber === move ? 'active' : ''}>
           <button onClick={() => this.jumpTo(move)}>{desc}</button>
           {move !== 0 ? (
             <strong className="location">
