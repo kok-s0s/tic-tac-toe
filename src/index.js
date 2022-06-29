5880af60a0cb910b4a362ef60c18dccf8dc9de69
kok-s0s <2694308562@qq.com>
2022-06-30 00:08:48 +0800

    feat: use two loops to make the squares

--- src/index.js
+++ src/index.js
@@ -52,21 +52,15 @@ class Board extends React.Component {
 
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {[0, 1, 2].map((i) => {
+          return (
+            <div className="board-row" key={i}>
+              {[0, 1, 2].map((j) => {
+                return this.renderSquare(i * 3 + j)
+              })}
+            </div>
+          )
+        })}
       </div>
     )
   }
